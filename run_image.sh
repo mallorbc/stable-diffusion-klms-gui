2488b94ce317ca19711714cea81584320c80377f
sam-or <67568065+sam-or@users.noreply.github.com>
2022-09-07 17:35:57 +1000

    Quote path

--- run_image.sh
+++ run_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p ./log
-docker run --rm -p 7860:7860 --gpus all -v `pwd`/diffusion_model:/app/models/ldm/stable-diffusion-v1/ -v `pwd`/outputs:/app/outputs --name stable_diffusion --ipc host stable_diffusion 
+docker run --rm -p 7860:7860 --gpus all -v "`pwd`/diffusion_model":/app/models/ldm/stable-diffusion-v1/ -v "`pwd`/outputs":/app/outputs --name stable_diffusion --ipc host stable_diffusion 
